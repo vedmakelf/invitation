ecf3d17f6e804ffe07f93977798d65bfa0253d6e
vedmakelf <matveychukivan.r@gmail.com>
2018-04-27 16:45:08 +0300

    переменование папки

---
+++ docs/backup/js/menu.js
@@ -0,0 +1,7 @@
+document.write(
+    <ul class="main-menu">
+        <li><a href="index.html">Главная</a></li>
+        <li><a href="confirmation.html">test</a></li>
+        <li><a href="">Галерея</a></li>
+    </ul>
+)
